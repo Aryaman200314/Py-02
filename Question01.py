343fea0bedb28b2da7ed954f4b76dae52ade047f
aryaman <200314arya@gmail.com>
2025-02-20 09:17:49 +0530

    changes

--- Question01.py
+++ Question01.py
@@ -2,16 +2,9 @@ import boto3
 
 def list_instance_types():
     ec2_client = boto3.client('ec2')
-    
-    try:
-        # Use paginator to handle multiple pages of results
-        paginator = ec2_client.get_paginator('describe_instance_types')
-        
-        for page in paginator.paginate():
-            for instance_type in page['InstanceTypes']:
-                print(f"\nInstance Type: {instance_type['InstanceType']}")
-                
-    except Exception as e:
-        print(f"Error: {e}")
+    paginator = ec2_client.get_paginator('describe_instance_types')
+    for page in paginator.paginate():
+        for instance_type in page['InstanceTypes']:
+            print(f"\nInstance Type: {instance_type['InstanceType']}")
 
 list_instance_types()
