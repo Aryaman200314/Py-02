662deac30d5ca8855e451d17d903164acff6deb1
aryaman <200314arya@gmail.com>
2025-02-20 09:31:58 +0530

    update

--- Question01.py
+++ Question01.py
@@ -5,6 +5,8 @@ def list_instance_types():
     paginator = ec2_client.get_paginator('describe_instance_types')
     for page in paginator.paginate():
         for instance_type in page['InstanceTypes']:
+            with open('instance_types.txt', 'a') as f:
+                f.write(instance_type['InstanceType'] + '\n')
             print(f"\nInstance Type: {instance_type['InstanceType']}")
 
 list_instance_types()
